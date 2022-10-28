e3cb356cbb634b7f778024643a31ca567f69c8e3
pystander <67277708+pystander@users.noreply.github.com>
2022-10-28 16:34:03 +0800

    Update picoCTF practice

---
+++ ctf/picoCTF/Practice/information/hex.py
@@ -0,0 +1,5 @@
+import base64
+
+key = 'cGljb0NURnt0aGVfbTN0YWRhdGFfMXNfbW9kaWZpZWR9'
+
+print(base64.b64decode(key))
---
+++ ctf/picoCTF/Practice/mod-26/rot13.py
@@ -0,0 +1,4 @@
+import codecs
+
+key = r"cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_Ncualgvd}"
+print(codecs.encode(key, 'rot_13'))
